a69d1c0a5d6b112662ca7d8313be70b70d4bde87
midomido213 <midopoppo213@gmail.com>
2017-08-24 16:52:07 +0900

    OAuth認証まで実装

--- app/src/main/java/midomido213/midomidoclient/MainActivity.java
+++ app/src/main/java/midomido213/midomidoclient/MainActivity.java
@@ -1,7 +1,9 @@
 package midomido213.midomidoclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!TwitterUtils.hasAccessToken(this)) {
+            Intent intent = new Intent(this, TwitterOAuthActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
