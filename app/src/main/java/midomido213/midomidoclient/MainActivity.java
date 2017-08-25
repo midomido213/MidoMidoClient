2b40fc763994db8b9440ab2b4f1f6f7f6a7516d3
midomido213 <midopoppo213@gmail.com>
2017-08-26 00:21:03 +0900

    タイムライン表示　テキストだけ

--- app/src/main/java/midomido213/midomidoclient/MainActivity.java
+++ app/src/main/java/midomido213/midomidoclient/MainActivity.java
@@ -1,21 +1,86 @@
 package midomido213.midomidoclient;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class MainActivity extends ListActivity {
+
+    private TweetAdapter mAdapter;
+    private Twitter mTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         if (!TwitterUtils.hasAccessToken(this)) {
             Intent intent = new Intent(this, TwitterOAuthActivity.class);
             startActivity(intent);
             finish();
+        } else {
+            mAdapter = new TweetAdapter(this);
+            setListAdapter(mAdapter);
+
+            mTwitter = TwitterUtils.getTwitterInstance(this);
+            reloadTimeLine();
         }
+    }
+
+    private class TweetAdapter extends ArrayAdapter<String> {
+
+        public TweetAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_1);
+        }
+    }
+
+    private void reloadTimeLine() {
+        AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>() {
+            @Override
+            protected List<String> doInBackground(Void... params) {
+                try {
+                    ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
+                    ArrayList<String> list = new ArrayList<String>();
+                    for (twitter4j.Status status : timeline) {
+                        list.add(status.getText());
+                    }
+                    return list;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<String> result) {
+                if (result != null) {
+                    mAdapter.clear();
+                    for (String status : result) {
+                        mAdapter.add(status);
+                    }
+                    getListView().setSelection(0);
+                } else {
+                    showToast("タイムラインの取得に失敗しました。。。");
+                }
+            }
+        };
+        task.execute();
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
